cfb4b999012c981000e2eefb72db13ee728db37a
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2025-01-25 11:27:27 +0300

    line 73 have mistake

--- load_ing.py
+++ load_ing.py
@@ -1,10 +1,10 @@
 import pygame
 from script import load_image
 
-player_image = load_image('image/player/bottom')
-player_image = load_image('image/player/left')
-player_image =load_image('image/player/top')
-player_image = load_image('image/player/right')
+player_image_b = load_image('image/player/bottom')
+player_image_l = load_image('image/player/left')
+player_image_t =load_image('image/player/top')
+player_image_r = load_image('image/player/right')
 spider_image = load_image('image/spider')
 axe_image = pygame.image.load('image/topor/1.png').convert_alpha()
 block_image = pygame.image.load('image/block/block.jpg').convert_alpha()
--- main_ner.py
+++ main_ner.py
@@ -1,9 +1,10 @@
+
+
 import pygame
 import  os
 import sys
 import random
 from math import *
-
 
 
 
@@ -26,15 +27,16 @@ def restart():
     axe_group = pygame.sprite.Group()
     block_group = pygame.sprite.Group()
     water_group = pygame.sprite.Group()
-    player = Player(player_image,(0,0))
-    player.group.add(player)
+    player = Player(player_image_b,(100,100))
+    player_group.add(player)
+    player.add_axe()
 
 
 
 
 
 def gamelvl():
-    sc.fill('white')
+    sc.fill('gray')
     player_group.draw(sc)
     player_group.update()
     spider_group.draw(sc)
@@ -51,10 +53,27 @@ def gamelvl():
     pygame.display.update()
 
 
+def drawmaps(nameFile):
+    maps = []
+    source = "lvl/" + str(nameFile)
+    with open(source, "r") as file:
+        for i in range(0,100):
+            maps.append(file.readline().replace("\n", "").split(",")[0:-1])
 
-
-
-
+    pos = [0,0]
+    for i in range (0,len(maps)):
+        pos[1] = i * 40
+        for j in range(0,len(maps[0])):
+            pos[0] = 40 * j
+            if maps[i][j] == '1':
+                block = Block(block_image,pos)
+                block_group.add(block)
+            elif maps[i][j] == '2':
+                spawner = Spawner(spawnerImage,pos)
+                spawner_group.add(spawner)
+            elif maps[i][j] == '3':
+                water = Water(water_image,pos)
+                water_group.add(water)
 
 
 class Axe(pygame.sprite.Sprite):
@@ -67,7 +86,9 @@ class Axe(pygame.sprite.Sprite):
         self.deg_rotate = 0
         self.deg = start_deg
         self.timer_attack = 0
-
+    def update(self):
+        self.rotate()
+        self.move()
     def rotate(self):
         self.deg_rotate -= 20
         self.image = pygame.transform.rotate(axe_image,self.deg_rotate)
@@ -77,14 +98,37 @@ class Axe(pygame.sprite.Sprite):
     def move(self):
         self.deg += 3
         self.rect.centerx = 150 * cos(radians(self.deg)) + player.rect.centerx
-        self.rect.centery = 150 * sin(radians(self.deg)) + player.rect.centery
+        self.rect.centery = 80 * sin(radians(self.deg)) + player.rect.centery
 
 
 
 
 
+class Block(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
+class Spawner(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Water(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.image = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
 
@@ -94,19 +138,19 @@ class Axe(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
         def __init__(self, image, pos):
-            pygame.sprite.Sprite.__init__()
-            self.image = image
+            pygame.sprite.Sprite.__init__(self)
+            self.image = image[0]
             self.rect = self.image.get_rect()
             self.rect.x = pos[0]
             self.rect.y = pos [1]
             self.key = pygame.key.get_pressed()
-            self.speed = 10
+            self.speed = 5
             self.timer_anime = 0
             self.anime = False
             self.frame = 0
             self.pos_maps = [0,0]
             self.score = 0
-            self.axe = 1
+            self.axe = 3
             self.camera = False
 
 
@@ -115,9 +159,22 @@ class Player(pygame.sprite.Sprite):
             global axe_group
             axe_group = pygame.sprite.Group()
             for i in range(self.axe):
-                axe = Axe(axe_image,(self.rect.centerx + 70,self.rect.rectery + 70),(360// self.axe * i))
+                axe = Axe(axe_image,(self.rect.centerx + 70,self.rect.centery + 70),(360// self.axe * i))
                 axe_group.add(axe)
 
+        def update(self):
+            self.key = pygame.key.get_pressed()
+            if self.key[pygame.K_a]:
+                self.rect.x -= self.speed
+
+            elif self.key[pygame.K_d]:
+                self.rect.x += self.speed
+            elif self.key[pygame.K_w]:
+                self.rect.y -= self.speed
+            elif self.key[pygame.K_s]:
+                self.rect.y += self.speed
+            if self.key[pygame.K_r]:
+                restart()
 
 
 
@@ -177,10 +234,7 @@ class Player(pygame.sprite.Sprite):
 
 
 
-
-
-
-
+drawmaps('1.txt')
 restart()
 while True:
     for event in pygame.event.get():
